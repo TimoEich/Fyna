10777e862be52dd4a53d45e0ae287d4207c4d558
Timo Eich <timoei@outlook.de>
2021-03-26 16:34:34 +0100

    First Push

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="h-screen bg-gray-100 flex items-center justify-center">
+        <p class="uppercase text-9xl font-black">fyna.</p>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style>
+</style>
